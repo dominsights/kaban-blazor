f1cfae70d42b6e4acf1ce998998b097f553ecc42
Domício Medeiros <domicioam@gmail.com>
2022-04-28 21:28:55 +0100

    add more components and implement add board

--- Kanban/Program.cs
+++ Kanban/Program.cs
@@ -1,11 +1,15 @@
 using Kanban;
+using Kanban.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<KanbanApi>();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
--- Kanban/Services/KanbanApi.cs
+++ Kanban/Services/KanbanApi.cs
@@ -1,6 +1,26 @@
-﻿namespace Kanban.Services
+﻿using System.Net.Http.Json;
+using static Kanban.Pages.Boards;
+
+namespace Kanban.Services
 {
     public class KanbanApi
     {
+        private readonly HttpClient httpClient;
+
+        public KanbanApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<Board[]> GetAllBoards()
+        {
+            return await httpClient.GetFromJsonAsync<Board[]>("http://localhost:8080/board");
+        }
+
+        public async Task SaveBoard(CreateBoardRequest board)
+        {
+            await httpClient.PostAsJsonAsync<CreateBoardRequest>("http://localhost:8080/board", board);
+        }
     }
+
 }
